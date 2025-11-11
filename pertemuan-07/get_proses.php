281ebb3ae4c9b6617e4140a313821025e70d88fd
2511500021-wq <2511500021@mahasiswa.tmaluhur.ac.id>
2025-11-11 18:02:27 +0700

    kumpulan latihan

--- pertemuan-07/get_proses.php
+++ pertemuan-07/get_proses.php
@@ -1,7 +1,7 @@
 <?php
-session_start(); 
+session_start();
 $_SESSION["nama"] = $_GET["txtNama"];
 $_SESSION["email"] = $_GET["txtEmail"];
 $_SESSION["pesan"] = $_GET["txtPesan"];
-header (header: "location: get.php");
+header("location: get.php");
 ?>
